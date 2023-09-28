70b00cfd7bc111340066f9428ebd0b6bef3f64c1
Mahesh Adhikari <62585493+mahesh-gfx@users.noreply.github.com>
2023-09-28 23:10:48 +0100

    project structure

---
+++ base/components.py
@@ -0,0 +1,9 @@
+import tkinter as tk
+from tkinter import *
+from tkinter import font
+from tkinter.ttk import *
+
+# styled
+styledButton = Style()
+styledButton.configure('W.TButton', font=(
+    'calibri', 10, 'bold'), foreground='white')
