2563195b567473068a2621effcb53ee58b87f01b
Jzx-china <1484235995@qq.com>
2023-10-12 17:03:19 +0800

    ui

--- ProgSD_M5_EVShareApp/base/components.py
+++
@@ -1,9 +0,0 @@
-import tkinter as tk
-from tkinter import *
-from tkinter import font
-from tkinter.ttk import *
-
-# styled
-styledButton = Style()
-styledButton.configure('W.TButton', font=(
-    'calibri', 10, 'bold'), foreground='white')
--- ProgSD_M5_EVShareApp/base/fonts.py
+++
@@ -1,9 +0,0 @@
-import tkinter as tk
-from tkinter import *
-from tkinter import font
-from tkinter.ttk import *
-
-button = font.Font(size=16)
-body = font.Font(size=14)
-title = font.Font(size=22)
-subtitle = font.Font(size=18)
