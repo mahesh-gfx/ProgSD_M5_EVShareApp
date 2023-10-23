e903ce575df4b827892cb0dc4896f49a461357e0
Mahesh Adhikari <62585493+mahesh-gfx@users.noreply.github.com>
2023-10-23 15:05:30 +0100

    connect frames, vehicles view, welcome

--- main.py
+++ main.py
@@ -1,21 +1,48 @@
 import tkinter as tk
 from screens import welcome
-import sqlite3
-import pandas as pd
 from database import db
-from screens.welcome import welcome
+from screens.welcome import Welcome
+from screens.vehiclesView import VehiclesView
 
 
-class App():
+class App(tk.Tk):
     # Attributes
     vehicles = []
     db_name = 'zevo-dev.db'
-
     # Constructors
+
     def __init__(self):
-        print("Constructing App...")
+        super().__init__()
+        self.geometry("480x800")
+        self.title("Zevo | EV Rental")
+        self.resizable(False, True)
         self.database = db()
-        self.welcome = welcome()
+
+        container = tk.Frame(self)
+        container.pack(side="top", fill="both", expand=True)
+        container.grid_rowconfigure(0, weight=1)
+        container.grid_columnconfigure(0, weight=1)
+
+        print("Constructing App...")
+
+        self.activeFrames = {}
+        self.allFrames = {'welcome': Welcome,
+                          'vehiclesView': VehiclesView}
+
+        for key in self.allFrames:
+            print('Key: ', key)
+            frame = self.allFrames[key](container, controller=self)
+            self.activeFrames[key] = frame
+            frame.grid(row=0, column=0, sticky="nsew")
+
+        self.change_frame('vehiclesView')
+
+    def change_frame(self, pageName):
+        frame = self.activeFrames[pageName]
+        frame.tkraise()
+        print('Changed Frame to ', pageName)
 
 
-a = App()
+if __name__ == "__main__":
+    app = App()
+    app.mainloop()
