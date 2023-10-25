c64e6e226c776c41121550704c95e77b6a50e3c8
Mahesh Adhikari <62585493+mahesh-gfx@users.noreply.github.com>
2023-10-25 02:06:17 +0100

    Connected welcome, vehicles view, vehicle details

--- main.py
+++ main.py
@@ -1,14 +1,38 @@
 import tkinter as tk
-from screens import welcome
 from database import db
 from screens.welcome import Welcome
 from screens.vehiclesView import VehiclesView
+from screens.vehicleDetails import VehicleDetails
 
 
 class App(tk.Tk):
     # Attributes
     vehicles = []
     db_name = 'zevo-dev.db'
+    selectedVehicle = {
+        "type": "Car",
+        "vehicleClass": "SUV",
+        "make": "Tesla",
+        "model": "Model S",
+        "licensePlateNumber": "XYZ 123",
+        "ratePerWeek": "20",
+        "ratePerDay": "4",
+        "ratePerHour": "0.2",
+        "batteryCapacity": "64kWh",
+        "range": "550",
+        "doors": "5",
+        "seatingCapacity": "7",
+        "horsepower": "200",
+        "maxSpeed": "310",
+        "inUse": False,
+        "atSite": True,
+        "history": [],
+        "defects": [],
+        "image": "blue-tesla",
+        "bg": "#04317D",
+        "fg": "#FFFFFF",
+        "distance": "0.2",
+    }
     # Constructors
 
     def __init__(self):
@@ -18,29 +42,39 @@ class App(tk.Tk):
         self.resizable(False, True)
         self.database = db()
 
-        container = tk.Frame(self)
-        container.pack(side="top", fill="both", expand=True)
-        container.grid_rowconfigure(0, weight=1)
-        container.grid_columnconfigure(0, weight=1)
+        self.container = tk.Frame(self)
+        self.container.pack(side="top", fill="both", expand=True)
+        self.container.grid_rowconfigure(0, weight=1)
+        self.container.grid_columnconfigure(0, weight=1)
 
         print("Constructing App...")
 
         self.activeFrames = {}
         self.allFrames = {'welcome': Welcome,
-                          'vehiclesView': VehiclesView}
+                          'vehiclesView': VehiclesView,
+                          'vehicleDetails': VehicleDetails}
 
         for key in self.allFrames:
             print('Key: ', key)
-            frame = self.allFrames[key](container, controller=self)
+            frame = self.allFrames[key](self.container, controller=self)
             self.activeFrames[key] = frame
             frame.grid(row=0, column=0, sticky="nsew")
 
-        self.change_frame('vehiclesView')
+        self.change_frame('welcome')
 
     def change_frame(self, pageName):
         frame = self.activeFrames[pageName]
         frame.tkraise()
         print('Changed Frame to ', pageName)
+
+    # Getters
+    def get_selected_car_details():
+        return ""
+
+    # Setters
+    def set_selected_vehicle_details(self, vehicle):
+        print("Changing selected vehicle details...", vehicle)
+        self.selectedVehicle = vehicle
 
 
 if __name__ == "__main__":
--- screens/vehiclesView.py
+++ screens/vehiclesView.py
@@ -25,7 +25,10 @@ class VehiclesView(ttk.Frame):
         "atSite": True,
         "history": [],
         "defects":[],
-        "image":"blue-tesla"
+        "image":"blue-tesla",
+        "bg":"#04317D",
+        "fg":"#FFFFFF",
+        "distance":"0.2",
     },
         {
         "type": "Car",
@@ -46,7 +49,11 @@ class VehiclesView(ttk.Frame):
         "atSite": True,
         "history": [],
         "defects":[],
-        "image":"red-tesla"
+        "image":"red-tesla",
+        "bg":"#D22739",
+        "fg":"#FFFFFF",
+        "distance":"0.8",
+
     },
         {
         "type": "Car",
@@ -67,7 +74,11 @@ class VehiclesView(ttk.Frame):
         "atSite": True,
         "history": [],
         "defects":[],
-        "image":"white-tesla"
+        "image":"white-tesla",
+        "bg":"#ECEDED",
+        "fg":"#000000",
+        "distance":"0.7",
+
     },
         {
         "type": "Car",
@@ -88,7 +99,11 @@ class VehiclesView(ttk.Frame):
         "atSite": True,
         "history": [],
         "defects":[],
-        "image":"white-tesla"
+        "image":"white-tesla",
+        "bg":"#ECEDED",
+        "fg":"#000000",
+        "distance":"0.7",
+
     }, {
         "type": "Car",
         "vehicleClass": "SUV",
@@ -108,7 +123,10 @@ class VehiclesView(ttk.Frame):
         "atSite": True,
         "history": [],
         "defects":[],
-        "image":"white-tesla"
+        "image":"white-tesla",
+        "bg":"#ECEDED",
+        "fg":"#000000",
+        "distance":"0.2",
     }, {
         "type": "Car",
         "vehicleClass": "SUV",
@@ -128,7 +146,11 @@ class VehiclesView(ttk.Frame):
         "atSite": True,
         "history": [],
         "defects":[],
-        "image":"white-tesla"
+        "image":"white-tesla",
+        "bg":"#ECEDED",
+        "fg":"#000000",
+        "distance":"0.2",
+
     }, {
         "type": "Car",
         "vehicleClass": "SUV",
@@ -148,7 +170,10 @@ class VehiclesView(ttk.Frame):
         "atSite": True,
         "history": [],
         "defects":[],
-        "image":"white-tesla"
+        "image":"white-tesla",
+        "bg":"#ECEDED",
+        "fg":"#000000",
+        "distance":"0.2",
     }, {
         "type": "Car",
         "vehicleClass": "SUV",
@@ -168,7 +193,10 @@ class VehiclesView(ttk.Frame):
         "atSite": True,
         "history": [],
         "defects":[],
-        "image":"white-tesla"
+        "image":"white-tesla",
+        "bg":"#ECEDED",
+        "fg":"#000000",
+        "distance":"0.2",
     }, {
         "type": "Car",
         "vehicleClass": "SUV",
@@ -188,7 +216,10 @@ class VehiclesView(ttk.Frame):
         "atSite": True,
         "history": [],
         "defects":[],
-        "image":"white-tesla"
+        "image":"white-tesla",
+        "bg":"#ECEDED",
+        "fg":"#000000",
+        "distance":"0.2",
     }, {
         "type": "Car",
         "vehicleClass": "SUV",
@@ -208,7 +239,10 @@ class VehiclesView(ttk.Frame):
         "atSite": True,
         "history": [],
         "defects":[],
-        "image":"white-tesla"
+        "image":"white-tesla",
+        "bg":"#ECEDED",
+        "fg":"#000000",
+        "distance":"0.2",
     }, {
         "type": "Car",
         "vehicleClass": "SUV",
@@ -228,7 +262,10 @@ class VehiclesView(ttk.Frame):
         "atSite": True,
         "history": [],
         "defects":[],
-        "image":"white-tesla"
+        "image":"white-tesla",
+        "bg":"#ECEDED",
+        "fg":"#000000",
+        "distance":"0.2",
     }, {
         "type": "Car",
         "vehicleClass": "SUV",
@@ -248,7 +285,10 @@ class VehiclesView(ttk.Frame):
         "atSite": True,
         "history": [],
         "defects":[],
-        "image":"white-tesla"
+        "image":"white-tesla",
+        "bg":"#ECEDED",
+        "fg":"#000000",
+        "distance":"0.2",
     }, {
         "type": "Car",
         "vehicleClass": "SUV",
@@ -268,7 +308,10 @@ class VehiclesView(ttk.Frame):
         "atSite": True,
         "history": [],
         "defects":[],
-        "image":"white-tesla"
+        "image":"white-tesla",
+        "bg":"#ECEDED",
+        "fg":"#000000",
+        "distance":"0.2",
     }, {
         "type": "Car",
         "vehicleClass": "SUV",
@@ -288,13 +331,16 @@ class VehiclesView(ttk.Frame):
         "atSite": True,
         "history": [],
         "defects":[],
-        "image":"white-tesla"
+        "image":"white-tesla",
+        "bg":"#ECEDED",
+        "fg":"#000000",
+        "distance":"0.2",
     },
     ]
 
     def __init__(self, container, controller):
         super().__init__(container)
-        print("Constructing Vehicles View frame...")
+        print("Constructing Vehicles View self.navigation...")
         # self.configure(bg="white")
         self.style = ttk.Style()
         self.style.configure("TButton", font=("Helvetica", 16))
@@ -304,89 +350,126 @@ class VehiclesView(ttk.Frame):
         self.options = ['Sedan', 'Bike', 'Hatchback', 'Coupe', 'SUV']
         self.selected_vehicle.set(self.options[0])
 
-        locationIcon = tk.PhotoImage(
-            file="./image_components/location-drop.png")
-        self.locationButton = ttk.Button(self, text=self.locationButtonText, image=locationIcon, compound="left",
+        self.locationIcon = tk.PhotoImage(
+            file="./image_components/pin_duotone.png")
+        self.locationButton = ttk.Button(self, text=self.locationButtonText, image=self.locationIcon, compound="left",
                                          command=lambda: self.getLocation())
-        self.locationButton.place(x=35, y=45, height=42, width=260)
+        self.locationButton.place(x=200, y=45, height=42, width=260)
 
-        label = tk.Label(self, text="I want to rent a",
-                         font=("Helvetica", 20,))
-        label.place(x=25, y=132)
+        self.label = tk.Label(self, text="I want to rent a",
+                              font=("Helvetica", 20))
+        self.label.place(x=25, y=132)
 
         self.canvasLine = tk.Canvas(self, width=400, height=400)
         self.canvasLine.create_line(0, 100, 150, 100, fill="black", width=2)
         self.canvasLine.place(x=32, y=105)
 
-        dropdown = ttk.OptionMenu(
+        self.dropdown = ttk.OptionMenu(
             self, self.selected_vehicle, *self.options)
-        dropdown.place(x=35, y=164, height=40, width=150)
+        self.dropdown.place(x=35, y=164, height=40, width=150)
 
-        dropDownStyle = ttk.Style()
-        dropDownStyle.configure('TMenubutton', font=(
+        self.style = ttk.Style()
+        self.style.configure('TMenubutton', font=(
             'Helvetica', 18, "bold"), anchor='w', justify='left')
 
-        label.lift()
-        dropdown.lift()
+        self.label.lift()
+        self.dropdown.lift()
 
-        sortLabel = tk.Label(text='Sort vehicles')
+        sortLabel = tk.Label(self, text='Sort vehicles')
         sortLabel.place(x=341, y=140)
 
         self.selectedSortOption = tk.StringVar()
         self.sortMenuOptions = ['Default', 'Rate: High to Low', 'Rate: Low to High',
                                 'Range: High to Low', 'Range: Low to High']
         self.selectedSortOption.set(self.sortMenuOptions[0])
-        sortMenu = tk.OptionMenu(
+        self.sortMenu = tk.OptionMenu(
             self, self.selectedSortOption, *self.sortMenuOptions)
 
-        sortMenu.place(x=341, y=155, height=42, width=108)
+        self.sortMenu.place(x=341, y=155, height=42, width=108)
 
-        self.canvasCarsContainer = tk.Canvas(self)
-        self.canvasCarsContainer.place(x=35, y=250, height=300)
-        scrollbar = tk.Scrollbar(
-            self, orient="vertical", command=self.canvasCarsContainer.yview)
+        canvas = tk.Canvas(self)
+        canvas.place(x=25, y=230, height=480, width=480)
+
+        self.carsContainer = tk.Frame(canvas)
+        canvas.create_window((0, 0), window=self.carsContainer, anchor="nw")
+
+        scrollbar = tk.Scrollbar(self, orient="vertical", command=canvas.yview)
         scrollbar.pack(side="right", fill="y")
-        self.canvasCarsContainer.configure(yscrollcommand=scrollbar.set)
+        canvas.config(yscrollcommand=scrollbar.set)
 
-        carsContainer = tk.Frame(self.canvasCarsContainer)
-        self.canvasCarsContainer.create_window(
-            (0, 0), window=carsContainer, anchor="nw")
+        self.mapIconDark = tk.PhotoImage(
+            file="./image_components/map_duotone_dark.png")
 
-        # Create scrollable frames with buttons and names
-        frame_list = []
+        self.mapIconLight = tk.PhotoImage(
+            file="./image_components/map_duotone_light.png")
+
+        # Navigation bar
+        self.navigation = tk.Frame(self)
+        button1 = tk.Button(
+            self.navigation, text="Home", width=10, height=2)
+        button2 = tk.Button(
+            self.navigation, text="History", width=10, height=2)
+        button3 = tk.Button(
+            self.navigation, text="Log Out", width=10, height=2)
+
+        self.navigation.grid_columnconfigure(0, weight=1)
+        self.navigation.grid_columnconfigure(1, weight=1)
+        self.navigation.grid_columnconfigure(2, weight=1)
+
+        button1.grid(row=0, column=0, padx=5)
+        button2.grid(row=0, column=1, padx=5)
+        button3.grid(row=0, column=2, padx=5)
+
+        self.navigation.place(x=0, y=700, height=100, width=480)
+
         for car in self.cars:
-            scrollable_frame = tk.Frame(carsContainer)
-            scrollable_frame.grid(row=len(frame_list)+1,
-                                  column=0, padx=10, pady=10)
+            scrollable_frame = tk.Frame(self.carsContainer, )
 
-            cardBackgroundImage = tk.PhotoImage(
-                file="./image_components/" + car['image'] + '.png')
-            cardBackground = tk.Label(image=cardBackgroundImage)
-            cardBackground.place(x=0, y=0)
+            car_image = tk.PhotoImage(
+                file=f"./image_components/{car['image']}.png")
+            label_image = tk.Label(
+                scrollable_frame, image=car_image, cursor="hand2")
+            label_image.image = car_image
+            label_image.grid(row=0, column=0)
 
-            label_make = tk.Label(scrollable_frame, text=car["make"])
-            label_make .grid(row=0, column=0)
-            label_model = tk.Label(scrollable_frame, text=car["model"])
-            label_model.grid(row=1, column=0)
+            label_make = tk.Label(
+                scrollable_frame, text=car["make"] + " " + car["model"], bg=car["bg"], fg=car["fg"], font=(
+                    'Helvetica', 18, "bold"), anchor='w', justify='left')
+            label_make.grid(row=0, column=0, sticky='nw', padx=30, pady=30)
+
+            self.label_distance_icon = tk.Label()
+
+            if (car['fg'] == '#000000'):
+                self.label_distance_icon = tk.Label(
+                    scrollable_frame, image=self.mapIconDark, bg=car["bg"])
+            else:
+                self.label_distance_icon = tk.Label(
+                    scrollable_frame, image=self.mapIconLight, bg=car["bg"])
+
+            self.label_distance_icon.grid(
+                row=0, column=0, sticky='nw', padx=30, pady=60)
+
+            label_distance = tk.Label(
+                scrollable_frame, text=car["distance"]+" miles away", bg=car["bg"], fg=car["fg"], font=(
+                    'Helvetica', 12), anchor='w', justify='left')
+            label_distance.grid(row=0, column=0, sticky='nw', padx=62, pady=62)
+
             label_seats = tk.Label(
-                scrollable_frame, text=car["seatingCapacity"])
-            label_seats.grid(row=2, column=0)
+                scrollable_frame, text="Seats "+car["seatingCapacity"], bg=car["bg"], fg=car["fg"], font=(
+                    'Helvetica', 12), anchor='w', justify='left')
+            label_seats.grid(row=0, column=0, sticky='nw', padx=30, pady=88)
 
-            button = tk.Button(scrollable_frame, text="Print Name",
-                               command=lambda n=car: self.print_name(n))
-            button.grid(row=3, column=0)
+            label_rate = tk.Label(
+                scrollable_frame, text="£"+car["ratePerWeek"] + "/week", bg=car["bg"], fg=car["fg"], font=(
+                    'Helvetica', 14, 'bold'), anchor='w', justify='left')
+            label_rate.grid(row=0, column=0, sticky='sw', padx=30, pady=50)
 
-            frame_list.append(scrollable_frame)
+            label_image.bind(
+                "<Button-1>", lambda event: self.handle_click_on_vehicle(controller, car))
+            scrollable_frame.pack()
 
-        carsContainer.bind(
-            "<Configure>", lambda event: self.on_configure(self, event))
-
-
-# Bind the canvas to configure the scrollable region
-
-
-    def on_configure(self, event):
-        self.canvas.configure(scrollregion=self.canvas.bbox("all"))
+        self.carsContainer.update_idletasks()
+        canvas.config(scrollregion=canvas.bbox("all"))
 
     def getLocation(self):
         self.after(4000, lambda: self.locationButton.config(
@@ -397,8 +480,10 @@ class VehiclesView(ttk.Frame):
         self.locationButton.config(
             text=self.locationButtonText)
 
-    def print_name(name):
-        print("Name:", name['model'])
+    def handle_click_on_vehicle(self, controller, car):
+        print("Clicked on label...")
+        controller.set_selected_vehicle_details(car)
+        controller.change_frame('vehicleDetails')
 
 
 # # single screen development
